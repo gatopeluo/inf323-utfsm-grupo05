4bf55699b298607d3bf5c86405de0a44b270c540
Germán Treimun <german.treimun@alumnos.usm.cl>
2017-12-20 00:54:34 -0300

    django con s3 en máquina master

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -30,6 +30,24 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+#AWS credentials
+
+try:
+    from djangotest.credentials import *
+except ImportError:
+    pass
+
+assert len(SECRET_KEY) > 20, 'Please set SECRET_KEY in credentials.py'
+
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
